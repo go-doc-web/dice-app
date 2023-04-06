d6c568b0b72c3d50967035af29481d6a804e8091
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-04-06 17:26:49 +0300

    end

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import { rollDice, leave, initGame } from './js/logicApp';
 //Game innitial conditions
 initGame();
 
-// Hide the dice element
-
-// Roll the dice
-
 refs.btnRoll.addEventListener('click', rollDice);
 refs.btnHold.addEventListener('click', leave);
 refs.btnNewGame.addEventListener('click', initGame);
